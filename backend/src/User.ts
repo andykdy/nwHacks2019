c2d84c418b69d0ec25e37115d6869e6ea17fa6de
Andy Kim <andykdy@hotmail.com>
2019-01-26 16:23:14 -0800

    Commiting user stuff for check

---
+++ backend/src/User.ts
@@ -0,0 +1,22 @@
+export default class user{
+    private userID: string;
+    private points: number;
+    private badges: string[];
+    private rank: string; // Should this also be another class?
+
+    constructor(userID: string ){
+        this.userID = userID;
+        this.points = 0;
+        this.badges = [];
+        this.rank = "loser";
+    }
+
+    public get_user_ID(): string{
+        return this.userID;
+    }
+
+    public get_points(): number{
+        return this.points;
+    }
+
+}
---
+++ backend/src/UserManager.ts
@@ -0,0 +1,19 @@
+import user from "../src/User";
+
+class userManager{
+    private user_list: user[];
+
+    public get_users(): user[]{
+
+    }
+
+    public add_score(userID, score): {
+
+    }
+
+    public add_new_user(name): user[]{
+
+    }
+
+
+}
