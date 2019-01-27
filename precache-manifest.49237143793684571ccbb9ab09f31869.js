a800f43e2f9994e050d41c35bffe618f4e68f418
Andy Kim <andykdy@hotmail.com>
2019-01-27 06:33:20 -0800

    Updates

---
+++ precache-manifest.49237143793684571ccbb9ab09f31869.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d3db4a712bfe1a88a45",
+    "url": "/nwHacks2019/static/js/runtime~main.2d3db4a7.js"
+  },
+  {
+    "revision": "5b8009545d8bd13a9dc8",
+    "url": "/nwHacks2019/static/js/main.5b800954.chunk.js"
+  },
+  {
+    "revision": "71fb55414afe0a281fd2",
+    "url": "/nwHacks2019/static/js/1.71fb5541.chunk.js"
+  },
+  {
+    "revision": "5b8009545d8bd13a9dc8",
+    "url": "/nwHacks2019/static/css/main.5dd957a9.chunk.css"
+  },
+  {
+    "revision": "66866d4744472490cf03f14359042985",
+    "url": "/nwHacks2019/index.html"
+  }
+];
